a9a9d4bc352a4ca4a148f77507c33821ba28b919
Olivia Dresch <84184891+olivia-74@users.noreply.github.com>
2023-10-20 10:51:31 -0300

    Add files via upload

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+// Módulo de configuração da webapi, módulo de aplicação
+
+// Importar o pacote express (servidor)
+const express = require('express');
+
+// Responsável por lidar com requisições externas
+const cors = require('cors');
+
+// Importar as rotas para serem executadas na aplicação
+const userRouter = require('./routes/userRouter');
+const loginRouter = require('./routes/loginRouter');
+
+// Importar o pacote dotenv, gerenciador de variáveis de ambiente
+const dotenv = require('dotenv').config();
+
+// Instanciar o express na variável app
+const app = express();
+
+// Habilitar o recebimento de requests em formato JSON
+app.use(express.json());
+
+// Habilitar o recebimento de requests em formato JSON
+app.use(cors())
+
+// Habilitar as rotas na aplicação
+app.use('/api', userRouter);
+app.use('/api/auth', loginRouter);
+
+// Setar a porta do servidor, a partir do arquivo .env
+app.set('port', process.env.PORT || 1903);
+
+module.exports = app;
